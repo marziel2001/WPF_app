7335f44d30d1b3f739058b273716ba05d98a4ce1
Marcel <marziel2001@gmail.com>
2023-03-04 22:25:47 +0100

    Added context menus but dialog window still not showing in VSC

---
+++ CreateFileWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Navigation;
+using MenuItem = System.Windows.Controls.MenuItem;
+using System.Text.RegularExpressions;
+
+
+
+namespace lab2_WPF_app
+{
+    /// <summary>
+    /// Logika interakcji dla klasy CreateFileWindow.xaml
+    /// </summary>
+
+    public partial class CreateFileWindow : Window
+    {
+        private string path;
+        private bool success;
+        private string name;
+        public CreateFileWindow(string path)
+        {
+            InitializeComponent();
+            this.path = path;
+            success = false;
+        }
+
+        public void OK_Button(object sender, RoutedEventArgs a)
+        {
+            bool isDirectory = (bool)radioDirectory.IsChecked;
+            bool isFile = (bool)radioDirectory.IsChecked;
+
+            if (isFile && !Regex.IsMatch(CreateFileWindowName.Text, "^[a-zA-Z0-9-~_]{1,8}\\.(html|php|txt)$"))
+            {
+                System.Windows.MessageBox.Show("Incorrect name.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+            else if (!isFile && !isDirectory)
+            {
+                System.Windows.MessageBox.Show("Please choose over File or Directory", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+            else
+            {
+                Name = CreateFileWindowName.Text;
+                path = path + "\\" + Name;
+                FileAttributes attributes = FileAttributes.Normal;//assigns default mask
+                if ((bool)checkBoxR.IsChecked)
+                {
+                    attributes |= FileAttributes.ReadOnly;
+                }
+                if ((bool)checkBoxA.IsChecked)
+                {
+                    attributes |= FileAttributes.Archive;
+                }
+                if ((bool)checkBoxH.IsChecked)
+                {
+                    attributes |= FileAttributes.Hidden;
+                }
+                if ((bool)checkBoxS.IsChecked)
+                {
+                    attributes |= FileAttributes.System;
+                }
+                if (isFile)
+                {
+                    File.Create(path);
+                }
+                else if (isDirectory)
+                {
+                    Directory.CreateDirectory(path);
+                }
+                File.SetAttributes(path, attributes);
+                success = true;
+                Close();
+            }
+        }
+
+        public bool Succeeded()
+        { 
+            return success;
+        }
+
+        private void Cancel_Button(object sender, RoutedEventArgs a)
+        {
+            Close();
+        }
+
+        public string GetPath()
+        {
+            return path;
+        }
+
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -53,7 +53,15 @@ namespace lab2_WPF_app
                 Tag = directory.FullName
             };
 
-            foreach(DirectoryInfo subDirectory in directory.GetDirectories())
+            root.ContextMenu = new System.Windows.Controls.ContextMenu();//creates context menu when RMB clicked
+            var contextItem1 = new MenuItem { Header = "Create" };//creates button element
+            contextItem1.Click += new RoutedEventHandler(ContextItemCreate);// assigns proper action to the button
+            var contextItem2 = new MenuItem { Header = "Delete" };
+            contextItem2.Click += new RoutedEventHandler(ContextItemDelete);
+            root.ContextMenu.Items.Add(contextItem1);// adds Create and Delete buttons to context menu of tree's leaf
+            root.ContextMenu.Items.Add(contextItem2);
+
+            foreach (DirectoryInfo subDirectory in directory.GetDirectories())
             {
                 root.Items.Add(CreateTreeDirectory(subDirectory));
             }
@@ -72,11 +80,83 @@ namespace lab2_WPF_app
                 Tag = file.FullName
             };
 
+            item.ContextMenu = new System.Windows.Controls.ContextMenu();
+            var contextItem1 = new MenuItem { Header = "Open" };
+            contextItem1.Click += new RoutedEventHandler(ContextItemOpen);
+            var contextItem2 = new MenuItem { Header = "Delete" };
+            contextItem2.Click += new RoutedEventHandler(ContextItemDelete);
+            item.ContextMenu.Items.Add(contextItem1);
+            item.ContextMenu.Items.Add(contextItem2);
+            //item.Selected += new RoutedEventHandler(StatusBarUpdate);
             return item; 
+        }
+
+        //creates some file inside the directory
+        private void ContextItemCreate(object sender, RoutedEventArgs a)
+        {
+            TreeViewItem mainDirectory = (TreeViewItem)treeView.SelectedItem;
+            string path = (string)mainDirectory.Tag;
+            CreateFileWindow dialog = new CreateFileWindow(path); // creates dialog window for creating files
+            dialog.ShowDialog();
+
+            if(dialog.Succeeded())
+            {
+                if(File.Exists(dialog.GetPath()))
+                {
+                    FileInfo file = new FileInfo(dialog.GetPath());
+                    mainDirectory.Items.Add(CreateTreeFile(file));
+                }
+                else if(Directory.Exists(dialog.GetPath()))
+                {
+                    DirectoryInfo directory = new DirectoryInfo(dialog.GetPath());
+                    mainDirectory.Items.Add(CreateTreeDirectory(directory));
+                }
+            }
+        }
+
+        private void ContextItemDelete(object sender, RoutedEventArgs a)
+        {
+            TreeViewItem item = (TreeViewItem)treeView.SelectedItem;
+            string path = (string)item.Tag;
+            FileAttributes attributes = File.GetAttributes(path);
+            File.SetAttributes(path, attributes & ~FileAttributes.ReadOnly);
+
+            if((attributes & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                deleteDirectory(path);
+            }
+            else
+            {
+                File.Delete(path);
+            }
+
+            if( treeView.Items[0] != item)
+            {
+                TreeViewItem parent = (TreeViewItem)item.Parent;
+                parent.Items.Remove(item);
+            }
+            else
+            {
+                treeView.Items.Clear();
+            }
+        }
+        private void ContextItemOpen(object sender, RoutedEventArgs a)
+        {
+            TreeViewItem item = (TreeViewItem)treeView.SelectedItem;
+            string content = File.ReadAllText(((string)item.Tag));
+            //scrollViewer.Content = new TextBlock() { Text = content };
+        }
+
+        private void deleteDirectory(string path)
+        {
+
         }
         private void Close(object sender, RoutedEventArgs e) 
         {
             Close();
         }       
     }
+
+   
+
 }
